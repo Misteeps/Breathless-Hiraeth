e1e434879a2655ba34d7ee97c4ed6b830b391c6c
Misteeps <30960779+Misteeps@users.noreply.github.com>
2023-04-16 02:06:08 -0700

    Created fairy

---
+++ Sources/Respite X.1/Assets/Characters/Fairy/Fairy.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+using Simplex;
+
+
+namespace Game
+{
+    public class Fairy : MonoBehaviour
+    {
+        private ParticleSystem particles;
+
+        public int spinSpeed = 200;
+        public float SpinRadius
+        {
+            get => particles.shape.position.x;
+            set
+            {
+                ParticleSystem.ShapeModule shape = particles.shape;
+                new Transition(() => SpinRadius, value => shape.position = new Vector3(value, 0, 0), SpinRadius, value, "Fairy Spin").Curve(Function.Quadratic, Direction.Out, 400).Start();
+            }
+        }
+
+
+        void Start()
+        {
+            particles = GetComponent<ParticleSystem>();
+        }
+        void Update()
+        {
+            transform.eulerAngles += new Vector3(0, spinSpeed * Time.deltaTime, 0);
+
+            if (Input.GetKeyDown(KeyCode.BackQuote)) SpinRadius = 0;
+            if (Input.GetKeyDown(KeyCode.Alpha1)) SpinRadius = 1;
+            if (Input.GetKeyDown(KeyCode.Alpha2)) SpinRadius = 2;
+        }
+    }
+}
